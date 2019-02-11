3dbaff45a6bc1554457ffc5bbf499eadcf6b093b
Suvrat <etherjain@gmail.com>
2019-02-11 22:21:55 +0530

    added code

--- classify.py
+++ classify.py
@@ -15,15 +15,19 @@ def create_smaller():
   model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
 
-def load_train_data(train_data_path):
+def load_train_data(train_data_path,train_data_path_y):
     with open(train_data_path,"rb") as f:
         train_ = pickle.load(f)
-    train_len = train.shape[0]
-    
+
+    with open(train_data_path_y,"rb") as f:
+        train_y = pickle.load(f)
+
+    return train_,train_y
 estimators = []
 # estimators.append(('standardize', StandardScaler()))
 estimators.append(('mlp', KerasClassifier(build_fn=create_smaller, epochs=100, batch_size=4, verbose=0)))
 pipeline = Pipeline(estimators)
+train_data ,train_y_data = load_train_data("train_data.pickle","train_data_y.pickle")
 kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 results = cross_val_score(pipeline, train_data, train_y_data, cv=kfold)
 print("Smaller: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
